694aa8982410e6e336a078c0c744d1c641686203
marwenfray <fraymarwen@gmail.com>
2020-09-15 12:31:35 +0100

    marwen branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Houssem from './Houssem'
+import Marwen from './marwen'
 function App() {
   return (
     <div className="App">
+      <Marwen/>
      <Houssem/>
     </div>
   );
